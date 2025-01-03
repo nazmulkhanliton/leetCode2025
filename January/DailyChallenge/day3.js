bfccac125670427417dbdc0e9c78f11effde4e7c
nazmulkhanliton <liton079@gmail.com>
2025-01-04 02:47:49 +0600

    Day 3

---
+++ January/DailyChallenge/day3.js
@@ -0,0 +1,7 @@
+//2270. Number of Ways to Split Array
+var waysToSplitArray = function (nums) {};
+
+let nums = [10, 4, -8, 7];
+let result = waysToSplitArray(nums);
+console.log(result);
+//Output: 2;
