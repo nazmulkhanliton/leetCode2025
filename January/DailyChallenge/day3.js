2c03a4e6759435044bb681ac67c113a54a3e0739
nazmulkhanliton <liton079@gmail.com>
2025-01-04 03:11:05 +0600

    Day 3

--- January/DailyChallenge/day3.js
+++ January/DailyChallenge/day3.js
@@ -1,5 +1,18 @@
 //2270. Number of Ways to Split Array
-var waysToSplitArray = function (nums) {};
+var waysToSplitArray = function (nums) {
+  let leftSum = 0,
+    totalSum = nums.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
+  let splitCount = 0;
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    leftSum += nums[i];
+    if (leftSum >= totalSum - leftSum) splitCount++;
+  }
+  return splitCount;
+};
 
 let nums = [10, 4, -8, 7];
 let result = waysToSplitArray(nums);
