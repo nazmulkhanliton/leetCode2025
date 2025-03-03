fc3fdc6dbacde71be82d4d40d912ae7df93032cd
nazmulkhanliton <liton079@gmail.com>
2025-03-03 12:38:44 +0600

    Day 7

--- February/ProgrammingSkills/Day4.js
+++
@@ -1,9 +0,0 @@
-//242. Valid Anagram
-var isAnagram = function (s, t) {
-  return s.split("").sort().join("") === t.split("").sort().join("");
-};
-let s = "anagram",
-  t = "nagaram";
-let result = isAnagram(s, t);
-console.log(result);
-//Output: true
