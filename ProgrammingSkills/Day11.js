a8586ab1d8f9c1c2681a10ebd84cbd27cb04d28f
nazmulkhanliton <liton079@gmail.com>
2025-03-09 16:27:32 +0600

    Day 11

---
+++ ProgrammingSkills/Day11.js
@@ -0,0 +1,31 @@
+// 13. Roman to Integer
+var romanToInt = function (s) {
+  const romanMap = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let total = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let current = romanMap[s[i]];
+    let next = romanMap[s[i + 1]];
+
+    if (next > current) {
+      total -= current;
+    } else {
+      total += current;
+    }
+  }
+  return total;
+};
+let s = "LVIII";
+let result = romanToInt(s);
+console.log(result);
+// Output: 58
+// Explanation: L = 50, V= 5, III = 3.
