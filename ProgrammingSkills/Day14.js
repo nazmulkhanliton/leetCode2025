e186f18409504d85615ff023a8b1c4bec9b3184e
nazmulkhanliton <liton079@gmail.com>
2025-04-07 12:00:36 +0600

    Day 14

---
+++ ProgrammingSkills/Day14.js
@@ -0,0 +1,20 @@
+//1275. Find Winner on a Tic Tac Toe Game
+var tictactoe = function (moves) {
+  for (let i = 0; i < moves.length; i++) {
+    if (i % 2 == 0) {
+    }
+  }
+};
+let moves = [
+  [0, 0],
+  [2, 0],
+  [1, 1],
+  [2, 1],
+  [2, 2],
+];
+let result = tictactoe(moves);
+console.log(result);
+//Output: "A"
+// [00 01 02]
+// [10 11 12]
+// [20 21 22]
